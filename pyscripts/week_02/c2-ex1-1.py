63e0d9ab6e24c8f4012f7e90bbb2f4702af79c10
Kevin D <bdxbot@gmail.com>
2020-02-11 15:51:10 -0800

    finished lesson 1

--- pyscripts/week_02/c2-ex1-1.py
+++ pyscripts/week_02/c2-ex1-1.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+'''
+Use the extended 'ping' command and Netmiko on the 'cisco4' router. This should prompt you for additional information as follows:
+
+cisco4#ping
+Protocol [ip]: 
+Target IP address: 8.8.8.8
+Repeat count [5]: 
+Datagram size [100]: 
+Timeout in seconds [2]: 
+Extended commands [n]: 
+Sweep range of sizes [n]: 
+Type escape sequence to abort.
+Sending 5, 100-byte ICMP Echos to 8.8.8.8, timeout is 2 seconds:
+!!!!!
+Success rate is 100 percent (5/5), round-trip min/avg/max = 1/1/4 ms
+
+a. Use send_command_timing() to handle the additional prompting from this 'ping' command. Specify a target IP address of '8.8.8.8'
+
+b. Use send_command() and the expect_string argument to handle the additional prompting. Once again specify a target IP address of '8.8.8.8'.
+'''
+
 from netmiko import ConnectHandler
 from getpass import getpass
 
@@ -18,5 +39,20 @@ hosts_dict = [host_cisco4]
 host_connect = ConnectHandler(**host_cisco4)
 
 # print(host_connect.find_prompt())
+# print(host_connect.send_command('ping 8.8.8.8'))
 
-print(host_connect.send_command('ping 8.8.8.8'))
+output = host_connect.send_command_timing('ping')
+print(output)
+output += host_connect.send_command_timing('\n')
+print(output)
+output += host_connect.send_command_timing('8.8.8.8')
+print(output)
+output += host_connect.send_command_timing('\n')
+output += host_connect.send_command_timing('\n')
+output += host_connect.send_command_timing('\n')
+output += host_connect.send_command_timing('\n')
+output += host_connect.send_command_timing('\n')
+
+host_connect.disconnect()
+
+print(output)
